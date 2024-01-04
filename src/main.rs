bfeb90657022ab1771e383ba0c6334e493da7c9a
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-01-04 05:24:26 +0000

    Patch: Setup Cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// The config file to use, defaults to $HOME/.config/morfo/config.toml
+    #[arg(
+        long,
+        default_value = "$HOME/.config/morfo/config.toml",
+        value_name = "config"
+    )]
+    config: String,
+
+    /// Display all the build steps
+    #[arg(short, long, default_value = "false")]
+    verbose: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("morfo {}", args.verbose);
 }
