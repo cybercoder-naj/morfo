e5806194c52838ee583197b052dad0b5bd1a4aca
Nishant Aanjaney Jalan <cybercoder.nishant@gmail.com>
2024-01-14 02:41:31 +0000

    Rearranged code

--- src/act.rs
+++ src/act.rs
@@ -1,8 +1,3 @@
-use std::{error::Error, fs::create_dir, path::Path, process::Command};
-
-use crate::config::Config;
-use crate::utils;
-
 #[derive(Debug, PartialEq)]
 pub struct ACT {
     pub name: String,
@@ -12,7 +7,7 @@ pub struct ACT {
 }
 
 impl ACT {
-    pub fn new(name: &str) -> Self {
+    fn new(name: &str) -> Self {
         Self {
             name: name.to_string(),
             header: Option::default(),
@@ -21,24 +16,11 @@ impl ACT {
         }
     }
 
-    pub fn build(&mut self, config: &Config) -> Result<(), Box<dyn Error>> {
-        // create .out directory if it doesn't exist
-        if !Path::new(".out").exists() {
-            create_dir(".out")?;
-        }
+    pub fn build(name: &str) -> Self {
+        let main = ACT::new(name);
 
-        // use command to print pwd
-        Command::new(config.get_cc())
-            .arg(config.get_cflags().join(" ").as_str())
-            .arg(&self.name)
-            .arg("-o")
-            .arg(config.get_build_dir().join(utils::file_name(&self.name)))
-            .status()?;
-
-        Ok(())
+        main
     }
-
-    // file name of a path without the extension
 }
 
 #[cfg(test)]
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use std::{error::Error, io::Write};
+use std::{error::Error, fs::create_dir, io::Write, path::Path, process::Command};
 
+use act::ACT;
 use config::Config;
 
 mod act;
@@ -11,15 +12,43 @@ pub fn execute<W: Write>(
     config: Config,
     out: &mut W,
 ) -> Result<(), Box<dyn Error>> {
-    let mut act = act::ACT::new(main_file);
-    act.build(&config)?;
+    let act = ACT::build(main_file);
+    compile(&act, &config)?;
 
     run(main_file, &config, out)?;
     Ok(())
 }
 
+pub fn compile(act: &ACT, config: &Config) -> Result<(), Box<dyn Error>> {
+    // create .out directory if it doesn't exist
+    if !Path::new(".out").exists() {
+        create_dir(".out")?;
+    }
+
+    for dependency in &act.dependencies {
+        compile(dependency, config)?;
+    }
+
+    // use command to print pwd
+    let status = Command::new(config.get_cc())
+        .arg(config.get_cflags().join(" ").as_str())
+        .arg(&act.name)
+        .arg("-o")
+        .arg(config.get_build_dir().join(utils::file_name(&act.name)))
+        .status()?;
+
+    if !status.success() {
+        return Err(format!("Error compiling {}", act.name).into());
+    }
+
+    Ok(())
+}
+
 fn run<W: Write>(main_file: &str, config: &Config, out: &mut W) -> Result<(), Box<dyn Error>> {
     let executable = config.get_build_dir().join(utils::file_name(main_file));
+    if !executable.exists() {
+        return Err(format!("Executable {} does not exist", executable.display()).into());
+    }
 
     // use command to invoke the executable
     let run_project = std::process::Command::new(executable)
@@ -39,17 +68,17 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_execute() {
         let mut out = Vec::new();
-        execute("main.morfo", ConfigBuilder::default().build(), &mut out);
+        execute(
+            "examples/hello_world/main.c",
+            ConfigBuilder::default().set_cc("gcc").build(),
+            &mut out,
+        )
+        .unwrap();
         assert_eq!(
             String::from_utf8(out).unwrap(),
-            vec![
-                "Executing main file: main.morfo",
-                "Config: Config { cc: \"\", cflags: [] }\n"
-            ]
-            .join("\n")
+            vec!["Hello World!", ""].join("\n")
         );
     }
 }
